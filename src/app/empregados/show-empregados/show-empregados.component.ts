b227aef3e75430d05a5d69f4dac86c058fd5489d
Ulisses Parola <ulisses.parola@gmail.com>
2022-06-05 22:12:58 -0300

    complementar

--- src/app/empregados/show-empregados/show-empregados.component.ts
+++ src/app/empregados/show-empregados/show-empregados.component.ts
@@ -22,6 +22,47 @@ export class ShowEmpregadosComponent implements OnInit {
 
   ngOnInit(): void {
     this.empregadoList$ = this.service.getEmpregadolist();
+    this.escolaridadeList$ = this.service.getEscolaridadeList();
+    this.funcaoList$ = this.service.getFuncaoList();
+    this.refreshEscolaridadeMap();
+    this.refreshFuncaoMap();
+  }
+
+  modalTitle:string = '';
+  activateAddEditEmpregadoComponent:boolean = false;
+  empregado:any;
+
+  modalAdd(){
+    this.empregado = {
+      id:0,
+      matricula:'',
+      nome:'',
+      dataDeNascimento: null,
+      funcaoId:null,
+      escolaridadeId:null
+    }
+    this.modalTitle = "Adicionar Empregado";
+    this.activateAddEditEmpregadoComponent = true;
+  }
+
+  refreshEscolaridadeMap(){
+    this.service.getEscolaridadeList().subscribe(data =>{
+        this.escolaridadeList = data;
+
+        for (let i = 0; i < data.length; i++){
+          this.escolaridadeMap.set(this.escolaridadeList[i].id, this.escolaridadeList[i].nivelEscolaridade);
+        }
+    })
+  }
+
+  refreshFuncaoMap(){
+    this.service.getFuncaoList().subscribe(data =>{
+      this.funcaoList = data;
+
+      for (let i = 0; i < data.length; i++){
+        this.funcaoMap.set(this.funcaoList[i].id, this.funcaoList[i].nomeFuncao);
+      }
+  })
   }
 
 }
